1fa712b3d2c2324cd1b68317ec74515e64585430
penglei <stone6123@outlook.com>
2025-01-21 13:54:26 +0800

    kthSmallest

---
+++ solution/230_kth_smallest.c
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=c
+ *
+ * [230] 二叉搜索树中第 K 小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+#include "230_kth_smallest.h"
+#include "pea_stack.h"
+#include <stddef.h>
+
+int kthSmallest(struct TreeNode* root, int k)
+{
+    PeaStack_t *pStack = peaStackCreate(1000, sizeof(struct TreeNode *));
+    struct TreeNode *pNode = root;
+    int cnt = 0;
+    int res = -1;
+
+    while (pNode != NULL || pStack->pfEmpty(pStack) == false) {
+        while (pNode != NULL) {
+            pStack->pfPush(pStack, &pNode);
+            pNode = pNode->left;
+        }
+
+        pNode = *(struct TreeNode **)pStack->pfTop(pStack);
+        pStack->pfPop(pStack);
+        cnt++;
+        if (cnt == k) {
+            res = pNode->val;
+            goto l_end;
+        }
+        pNode = pNode->right;
+    }
+
+l_end:
+    pStack->pfDestroy(pStack);
+    return res;
+}
+// @lc code=end
+
---
+++ solution/230_kth_smallest.h
@@ -0,0 +1,8 @@
+#ifndef __230_KTH_SMALLEST_H__
+#define __230_KTH_SMALLEST_H__
+
+#include "pea_type.h"
+
+int kthSmallest(struct TreeNode* root, int k);
+
+#endif
